c71bb542cfd72b521b7c89e4c4428d230c2a900f
zhuo1ang <zhuo1angT@Outlook.com>
2020-02-23 17:57:58 +0800

    lab01

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 This repo is my labs & homework of CS61A@Berkeley.
 
 ---
+
+Lab00, HW01, Lab01 finished.
---
+++ lab01/lab01.py
@@ -0,0 +1,31 @@
+"""Lab 1: Expressions and Control Structures"""
+
+def both_positive(a, b):
+    """Returns True if both a and b are positive.
+
+    >>> both_positive(-1, 1)
+    False
+    >>> both_positive(1, 1)
+    True
+    """
+    return a > 0 and b > 0 # You can replace this line!
+
+def sum_digits(x):
+    """Sum all the digits of x.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    "*** YOUR CODE HERE ***"
+    sum = 0
+    while x != 0:
+        sum += x % 10
+        x //= 10
+    return sum
